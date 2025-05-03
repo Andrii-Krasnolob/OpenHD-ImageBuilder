8f1b241eb54d4233de45f53ba64e72c58525c2a4
root <root@S410G2-AT1100.>
2025-05-03 15:55:39 +0000

    replace fastmirror

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -21,6 +21,10 @@ function install_x20_packages {
 }
 # Raspbian-specific code
 function install_raspbian_packages {
+    rm -Rf /etc/apt/sources.list.d/raspi.list
+    rm -Rf /etc/apt/sources.list
+    echo 'deb http://mirror.ox.ac.uk/sites/archive.raspbian.org/archive/raspbian bullseye main contrib non-free rpi' >/etc/apt/sources.list
+    echo 'deb http://mirrors.ustc.edu.cn/archive.raspberrypi.org/debian/ bullseye main' >/etc/apt/sources.list.d/raspi.list
     sudo apt update && apt remove -y dkms
     BASE_PACKAGES="openhd-sys-utils openhd qopenhd apt-transport-https apt-utils open-hd-web-ui"
     PLATFORM_PACKAGES_HOLD="raspberrypi-kernel libraspberrypi-dev libraspberrypi-bin libraspberrypi0 libraspberrypi-doc raspberrypi-bootloader"
